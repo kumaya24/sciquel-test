dda34b2be3d2b7e44218fb165a68858207b9acbb
kumaya24 <jacquiwang05xx@gmail.com>
2024-05-12 16:36:52 -0400

    Back end test including bonus question by Jacqui Wang.

--- src/app/api/comments/route.ts
+++ src/app/api/comments/route.ts
@@ -1,13 +1,73 @@
 import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient} from "@prisma/client";
 
+const prisma = new PrismaClient();
+
+type CreateUserResponse = {
+    name:       string;
+    email:      string;
+    comment:    string;
+    createdAt:  string;
+  };
 
 export async function GET(req: NextRequest){
-    // fill here in
+   try{
+    const startIndex = parseInt(req.nextUrl.searchParams.get('StartIndex') || '0');
 
-    return NextResponse.json({ test: "success", }, { status: 200 })
-}
+    const posts = await prisma.post.findMany({
+        skip: startIndex,
+        take: 50
+    });
+    
+    return new NextResponse(JSON.stringify({ test: "success", posts }), {
+        status: 200,
+        headers: {
+            'Content-type': 'application/json',
+        }
+    });
+   } catch(error){
+        console.error(error);
+        return new NextResponse(JSON.stringify({error: "Failder to fetch posts."}), {
+            status: 500,
+            headers:{
+                'Content-Type': 'application/json',
+            }
+        });
+    }
+   }
 
 export async function POST(req: NextRequest){
-    // fill here in
-    return NextResponse.json({ test: "success", }, { status: 200 })
+    try{
+        const body = await req.json();
+        const { name, email, comment } = body;
+
+        //check whether have all required info
+        if(!name || !email ||!comment){
+            return new NextResponse(
+                JSON.stringify({
+                    status: 'error',
+                    message: 'Missing required field. Checked your name, email, and comment again.'
+                }), {
+                    status: 400,
+                    headers: {
+                        'Content-Type': 'application/json',
+                    }
+                 });
+        }
+
+        //store successful data
+        return new NextResponse(
+            JSON.stringify({
+                status: 'success',
+                message:'Comment successfully submitted.'
+            }), {status: 200}
+        )
+    }catch(error){
+        return new NextResponse(
+            JSON.stringify({
+                status:'error',
+                message:'random errors'
+            }), {status: 500}
+        );
+    }
 }
--- src/app/api/comments/schema.ts
+++ src/app/api/comments/schema.ts
@@ -1,9 +1,17 @@
 import { z } from "zod";
 
 export const commentGetSchema = z.object({
-
-})
+    name: z.string().min(1, { message: "Name is required" }),
+    email: z.string().email({ message: "Invalid email address" }),
+    comment: z.string().min(1, {message: "Comment is required"})
+});
 
 export const commentPostSchema = z.object({
-    
-})
+    test: z.literal("success"),
+    posts: z.array(z.object({
+        id: z.number(),
+        title: z.string(),
+        content: z.string(),
+        createdAt: z.string()
+    }))
+});
